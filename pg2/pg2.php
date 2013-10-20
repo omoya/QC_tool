b5ffc6ee9a30f6608f2b179c3ea59a136cd85bcb
omoya <omoya@ibit.org>
2013-10-21 00:24:52 +0200

    Included export to csv capabilities to pg2.php

--- pg2/pg2.php
+++ pg2/pg2.php
@@ -25,9 +25,12 @@ echo '
 	<body>
 		<div id="main-wrap">';
 			// Echo the number of documents retrieved
-echo $cursor->count() . ' document(s) found. <br/>';
+			//TODO: format the link as a button or similar.
+			//Create a link to the file
+echo $cursor->count() . ' document(s) found&nbsp&nbsp';
+echo '<a href="QCexport.csv">Export to CSV</a>';
 
-// Keep echoing the static HTML lines. Including table div openening and headers.
+// Keep echoing the static HTML lines. Including table div openning and headers.
 echo '
 		</div>
 		<div class="CSSTableGenerator">
@@ -43,10 +46,14 @@ echo '
 	
 	//Intialize a counter. It will be used to give names to table elements dynamically
 	$count = 0;
+	//$csv_array = array();
+	
 	// Print each document in a table
 	// Iterate through the result set
 	// TODO: retrieve level from database. Need to create the routines to evaluate the level of an entry
 	foreach ($cursor as $obj) {
+		// Append data to the array that will be used to export data to CSV
+		$csv_array[] = array($obj['submission']['publication']['PMID'],$obj['submission']['publication']['title'],$obj['submission']['publication']['abstract'],$obj['submission']['publication']['keywords']);
 	// Increase by 1 the counter each iteration and convert to string
 		$count = $count + 1;
 		$count_str = strval($count);
@@ -79,14 +86,34 @@ echo '
 echo '
 			</tbody>
 				</table>
-			</div>	
+			</div>
 	</body>		
 </html>';
 
+// Export function. Writes the csv in current folder (server side)
+function array2csv(array &$array)
+{
+   if (count($array) == 0) {
+     return null;
+   }
+   ob_start();
+   $df = fopen("QCexport.csv", 'w');
+   fputcsv($df, array_keys(reset($array)));
+   foreach ($array as $row) {
+      fputcsv($df, $row);
+   }
+   fclose($df);
+   return ob_get_clean();   
+}
 
-//Echo the Java script. Quotes must be escaped with backslashes
+//Update or create the file in server
+array2csv($csv_array);
+
+
+//Echo the Java script.
 echo'
 <script language="Javascript">
+
 //This is a test
 function testing()	
 	{
@@ -112,14 +139,16 @@ function testing()
 	xmlhttp.open("GET","printA.php",true);
 	xmlhttp.send();
 	}
-	function openPage(nr)	
-		{
-			if (nr > 3){
-				window.location.href = "http://localhost/pg2/printA.php"
-			}
-			else {
-				window.location.href = "http://localhost/pg2/printB.php?pmid=12"
-			}
+	
+	// Another test
+function openPage(nr)	
+	{
+		if (nr > 3){
+			window.location.href = "http://localhost/pg2/printA.php"
+		}
+		else {
+			window.location.href = "http://localhost/pg2/printB.php?pmid=12"
+		}
 	function httpGet()
 	    {
 	    	alert("Hello3");
@@ -127,6 +156,33 @@ function testing()
 	    }
 			
 	}
+	
+//This the export to CSV function
+function exportCSV()	
+	{
+	alert("Exporting");
+	//document.getElementById("main-wrap").innerHTML = "Hello";
+	if (window.XMLHttpRequest)
+			{// code for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp=new XMLHttpRequest();
+			}
+	else
+			{// code for IE6, IE5
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+	xmlhttp.onreadystatechange=function()
+	  {
+	  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+	    {
+		//document.getElementById("main-wrap").innerHTML = xmlhttp.responseText;
+		alert("Done! Data exported to QCreport.csv");
+	    }
+	  }
+	//alert("Hello2");
+	// Calls the PHP function. Sends parameters via GET.
+	xmlhttp.open("GET","CSVexport.php",true);
+	xmlhttp.send();
+	}			
 
 
 </script>';
